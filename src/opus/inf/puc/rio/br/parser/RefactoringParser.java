5d3138f121cd46a41be5167a0d2dbb664b94d896
anacarlagb <ana.carlagb@gmail.com>
2019-12-06 17:50:58 -0300

    Get refactoring data #1

--- src/opus/inf/puc/rio/br/parser/RefactoringParser.java
+++ src/opus/inf/puc/rio/br/parser/RefactoringParser.java
@@ -11,9 +11,28 @@ public class RefactoringParser {
 	public List<Refactoring> parser(String refactoringsPath){
 		
 		List<Refactoring> refs = new ArrayList<Refactoring>();
+		//Get csv 
+		
+		//for 
+		  //GetLine 
+		    //GetRefactoring() 
+		    //refs.add(ref)
+		
 		
 		
 		return refs;
+	}
+	
+	private Refactoring getRefactoring(String refactoringLine){
+	    Refactoring ref;
+		//GetRefactoringType
+		//GetCommit 
+		//GetLastCommit
+		//GetCodeElements
+		//GetDetails 
+		
+            return ref; 
+	   
 	}
 
 }
