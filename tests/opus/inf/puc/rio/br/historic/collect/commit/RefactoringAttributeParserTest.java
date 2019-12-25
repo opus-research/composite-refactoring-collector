deef48f4a8b53792502d32514c1b7e452c2b5bc1
anacarlagb <ana.carlagb@gmail.com>
2019-12-25 09:37:58 -0300

    Test - Get refactorings #1

---
+++ tests/opus/inf/puc/rio/br/historic/collect/commit/RefactoringAttributeParserTest.java
@@ -0,0 +1,29 @@
+package opus.inf.puc.rio.br.historic.collect.commit;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.junit.Test;
+
+public class RefactoringAttributeParserTest {
+
+	
+	
+	// ------------------------ PATTERN 1 ----------------------------------//
+	@Test
+	public void shouldPushDownAttribute(){	
+	}
+	
+	@Test
+	public void shouldPullUpAttribute(){	
+	}
+	
+	@Test
+	public void shouldMoveAttribute(){	
+	}
+	
+	
+	// ------------------------ PATTERN 2 ----------------------------------//
+
+}
