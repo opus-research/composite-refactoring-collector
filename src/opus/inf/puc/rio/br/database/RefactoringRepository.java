984ad4a50f301bec6aad2227d1355d6d976aee9c
anacarlagb <ana.carlagb@gmail.com>
2021-11-23 10:36:22 -0300

    Configuration on database #1

--- src/opus/inf/puc/rio/br/database/RefactoringRepository.java
+++ src/opus/inf/puc/rio/br/database/RefactoringRepository.java
@@ -23,7 +23,7 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 
-public class RefactoringRepository {
+public class RefactoringRepository extends Repository{
 	
 	
 	//https://www.mongodb.com/blog/post/getting-started-with-mongodb-and-java-part-i
@@ -31,36 +31,11 @@ public class RefactoringRepository {
 	//https://mongodb.github.io/mongo-java-driver/3.5/driver/getting-started/quick-start-pojo/
 
 	public static void main(String[] args) {
-		MongoClient mongoClient;
 
-		if (args.length == 0) {
-			// connect to the local database server
-			mongoClient = MongoClients.create();
-		} else {
-			mongoClient = MongoClients.create(args[0]);
-		}
-
-		// create codec registry for POJOs
-		CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
-				fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-
-		// get handle to "mydb" database
-		MongoDatabase database = mongoClient.getDatabase("refactoringsdb").withCodecRegistry(pojoCodecRegistry);
-
-		// get a handle to the "people" collection
-		MongoCollection<Refactoring> collection = database.getCollection("refactorings", Refactoring.class);
-
-		// drop all the data in it
-		collection.drop();
-        List<Refactoring> refList = new ArrayList<Refactoring>();
-		Refactoring refactoring = new Refactoring("1", "test", null,"em", "test");
-        refList.add(refactoring);
-        collection.insertMany(refList);
 	}
 	
-	private static void connect() {
-	
-		
+	private void insertAllRefactorings(List<Refactoring> refList) {
+		refactorings().insertMany(refList);
 	}
 
 }
---
+++ src/opus/inf/puc/rio/br/database/Repository.java
@@ -0,0 +1,50 @@
+package opus.inf.puc.rio.br.database;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import opus.inf.puc.rio.br.model.refactoring.Refactoring;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+public class Repository {
+
+    private MongoCollection<Refactoring> refactorings;
+    private MongoDatabase database;
+
+    public static void init(String[] args) {
+        Repository repository = new Repository();
+        MongoClient mongoClient;
+
+        if (args.length == 0) {
+            // connect to the local database server
+            mongoClient = MongoClients.create();
+        } else {
+            mongoClient = MongoClients.create(args[0]);
+        }
+
+        // create codec registry for POJOs
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+        // get handle to "mydb" database
+        repository.database = mongoClient.getDatabase("refactoringsdb").withCodecRegistry(pojoCodecRegistry);
+
+        // get a handle to the "people" collection
+        //MongoCollection<Refactoring> collection = database.getCollection("composites", CompositeRefactoring.class);
+
+
+    }
+
+    protected MongoCollection<Refactoring> refactorings(){
+        if(refactorings == null){
+            refactorings = database.getCollection("refactorings", Refactoring.class);
+        }
+        return refactorings;
+    }
+}
