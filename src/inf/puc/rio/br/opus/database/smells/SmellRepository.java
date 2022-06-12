a33b5c743ce1db7b28e16907759e84755b26c571
anacarlagb <ana.carlagb@gmail.com>
2022-06-12 20:05:35 -0300

    Find smells of classes and methods

--- src/inf/puc/rio/br/opus/database/smells/SmellRepository.java
+++ src/inf/puc/rio/br/opus/database/smells/SmellRepository.java
@@ -1,8 +1,11 @@
 package inf.puc.rio.br.opus.database.smells;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCursor;
 import inf.puc.rio.br.opus.database.Repository;
 import inf.puc.rio.br.opus.model.smell.CodeSmell;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class SmellRepository extends Repository {
@@ -15,12 +18,42 @@ public class SmellRepository extends Repository {
         smells().insertMany(smellList);
     }
 
-
     public List<CodeSmell> getSmellsOfClassByCommit(String commit, String className) {
-        return null;
+
+        BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("commit", commit);
+        searchQuery.put("codeElement", className);
+        FindIterable<CodeSmell> smellsIterable =  smells().find(searchQuery);
+
+        return getSmellsFromIterable(smellsIterable);
     }
 
     public List<CodeSmell> getSmellsOfMethodByCommit(String commit, String methodName) {
-        return null;
+
+        BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("commit", commit);
+
+        BasicDBObject regexQuery = new BasicDBObject();
+        regexQuery.put("$regex", methodName);
+
+        searchQuery.put("codeElement", regexQuery);
+
+        FindIterable<CodeSmell> smellsIterable =  smells().find(searchQuery);
+        MongoCursor<CodeSmell> smellsCursor = smellsIterable.iterator();
+
+        return getSmellsFromIterable(smellsIterable);
+    }
+
+    private List<CodeSmell> getSmellsFromIterable(FindIterable<CodeSmell> smellsIterable){
+
+        MongoCursor<CodeSmell> smellsCursor = smellsIterable.iterator();
+
+        List<CodeSmell> smells = new ArrayList<>();
+        while(smellsCursor.hasNext()) {
+            CodeSmell smell = smellsCursor.next();
+            smells.add(smell);
+        }
+
+        return smells;
     }
 }
